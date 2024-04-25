7978aef0e3727beb26e1d089265ce267d7ff8753
Mizakson <maxwellizakson@gmail.com>
2024-04-25 11:54:24 -0400

    Add helper functions

--- script.js
+++ script.js
@@ -12,11 +12,92 @@ class Node {
 class Tree {
     constructor(arr) {
         this.arr = arr
-        this.root = buildTree(arr)
+        this.root = null
     }
 }
 
 
-function buildTree(arr) {
-    
+
+// 3 helper functions
+// removeDuplicates, mergeSort, merge
+
+
+function removeDuplicates(arr) {
+    let uniqueArr = []
+    arr.forEach(element => {
+        if (!uniqueArr.includes(element)) uniqueArr.push(element)
+    });
+    return uniqueArr
 }
+
+
+function mergeSort(arr) {
+
+    if (arr.length < 2) return arr
+
+    else {
+        let mid = Math.ceil(arr.length / 2)
+        let left = arr.slice(0, mid)
+        let right = arr.slice(mid)
+        return merge(mergeSort(left), mergeSort(right))
+    }
+
+}
+
+
+function merge(l, r) {
+
+    let result = []
+
+    while (l.length > 0 && r.length > 0) {
+        
+        // if left > right -- push right to result
+        // else push left to result
+
+        l[0] > r[0]
+        ? result.push(r.shift())
+        : result.push(l.shift());
+    }
+
+    return removeDuplicates([...result, ...l, ...r])
+
+}
+
+
+function buildTree(arr) {
+    return null  
+  }
+
+
+function prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+
+let tester = [1,5,6,788,345,1456,22,33,45,1,12]
+
+
+// function buildTree(arr, start, end) {
+
+//     if (start > end) return null
+
+//     var mid = (start + end) / 2
+//     var node = new Node(arr[mid])
+
+//     node.left = buildTree(arr, start, mid - 1)
+//     node.right = buildTree(arr, mid + 1, end)
+
+//     return node
+
+// }
+
+console.log(mergeSort(tester))
